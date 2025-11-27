e0099c6a4d2f9de47fa3cfe82b176df5b0ad261f
mihendy <kryukovm1khael@yandex.ru>
2025-11-27 15:09:58 +0500

    fix: upload_file_obj

--- backend/app/services/s3_service.py
+++ backend/app/services/s3_service.py
@@ -7,14 +7,14 @@ from botocore.exceptions import BotoCoreError, ClientError
 
 class AsyncS3Service:
     def __init__(
-        self,
-        access_key: str,
-        secret_key: str,
-        region: str,
-        endpoint: str,
-        public_read: bool = True,
-        use_ssl: bool = True,
-        public_endpoint: str = None,
+            self,
+            access_key: str,
+            secret_key: str,
+            region: str,
+            endpoint: str,
+            public_read: bool = True,
+            use_ssl: bool = True,
+            public_endpoint: str = None,
     ):
         self.session = aiobotocore.session.get_session()
         self.client_kwargs = {
@@ -46,14 +46,21 @@ class AsyncS3Service:
             return False
 
     async def upload_file_obj(self, file_object: BinaryIO, bucket_name: str, object_key: str) -> Optional[str]:
-        extra = {
-            "ContentType": getattr(file_object, "content_type", "application/octet-stream"),
-        }
+        extra_args = {}
+        content_type = getattr(file_object, "content_type", "application/octet-stream")
 
         if self.public_read:
-            extra["ACL"] = "public-read"
+            extra_args["ACL"] = "public-read"
+
+        file_object.seek(0)
         async with await self._get_client() as client:
-            await client.upload_fileobj(Fileobj=file_object, Bucket=bucket_name, Key=object_key, ExtraArgs=extra)
+            await client.put_object(
+                Bucket=bucket_name,
+                Key=object_key,
+                Body=file_object,
+                ContentType=content_type,
+                **extra_args
+            )
         return f"{self.public_endpoint}/{bucket_name}/{object_key}"
 
     async def download_file_obj(self, file_object: BinaryIO, bucket_name: str, object_key: str):
